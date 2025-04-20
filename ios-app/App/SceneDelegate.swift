59711dc6c0ff04475042179b2bd8efcff2979b98
DmitriiPl <Plighig@mail.ru>
2025-04-20 22:49:18 +0300

    lab5

--- ios-app/App/SceneDelegate.swift
+++ ios-app/App/SceneDelegate.swift
@@ -5,6 +5,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private var appCoordinator: AppCoordinator?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
         let window = UIWindow(windowScene: windowScene)
         self.window = window
         
